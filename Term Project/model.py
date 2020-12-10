d94bee3e2107bf194c3755737b746a43db8d0a13
tim <tim@example.com>
2020-12-10 11:57:14 +0800

    model update

--- Term Project/model.py
+++ Term Project/model.py
@@ -16,14 +16,11 @@ class Bert_BiLstm_Crf(nn.Module):
 		self.bert = BertModel.from_pretrained(pretrained_model_name)
 		# self.bert.eval()  # 知用来取bert embedding
 
-		# self.start_label_id = tokenizer.cls_token_id
-		# self.end_label_id =  tokenizer.sep_token_id
-
 		self.start_label_id = 25
 		self.end_label_id = 26
 
-		# self.transitions.data[self.start_label_id, :] = -10000
-		# self.transitions.data[:, self.end_label_id] = -10000
+		self.transitions.data[self.start_label_id, :] = -10000
+		self.transitions.data[:, self.end_label_id] = -10000
 		self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 		self.transitions.to(self.device)
 
@@ -38,40 +35,41 @@ class Bert_BiLstm_Crf(nn.Module):
 	def _forward_alg(self, feats):
 	
 		# this also called alpha-recursion or forward recursion, to calculate log_prob of all barX 
-	
+		# 針對一個序列(句子) 計算所有路徑的總分數 = log sum exp of score(x, y)
+
 		# T = self.max_seq_length
-		T = feats.shape[1]  
+		max_seq_len = feats.shape[1]  
 		batch_size = feats.shape[0]
 
 		# alpha_recursion,forward, alpha(zt)=p(zt,bar_x_1:t)
-		log_alpha = torch.Tensor(batch_size, 1, self.n_tags).fill_(-10000.).to(self.device)  #[batch_size, 1, 16]
+		log_alpha = torch.Tensor(batch_size, 1, self.n_tags).fill_(-10000.).to(self.device)  #[batch_size, 1, n_tags]
 		# normal_alpha_0 : alpha[0]=Ot[0]*self.PIs
 		# self.start_label has all of the score. it is log,0 is p=1
 		log_alpha[:, 0, self.start_label_id] = 0
 
 		# feats: sentances -> word embedding -> lstm -> MLP -> feats
 		# feats is the probability of emission, feat.shape=(1,tag_size)
-		for t in range(1, T):
+		for t in range(1, max_seq_len):
 		    log_alpha = (log_sum_exp_batch(self.transitions + log_alpha, axis=-1) + feats[:, t]).unsqueeze(1)
 
 		# log_prob of all barX
 		log_prob_all_barX = log_sum_exp_batch(log_alpha)
 		return log_prob_all_barX
 
-		def _score_sentence(self, feats, label_ids):
-			T = feats.shape[1]
-			batch_size = feats.shape[0]
+	def _score_sentence(self, feats, label_ids):
+		T = feats.shape[1]
+		batch_size = feats.shape[0]
 
-			batch_transitions = self.transitions.expand(batch_size,self.n_tags, self.n_tags)
-			batch_transitions = batch_transitions.flatten(1)
+		batch_transitions = self.transitions.expand(batch_size,self.n_tags, self.n_tags)
+		batch_transitions = batch_transitions.flatten(1)
 
-			score = torch.zeros((feats.shape[0],1)).to(self.device)
-			# the 0th node is start_label->start_word,the probability of them=1. so t begin with 1.
-			for t in range(1, T):
-			    score = score + \
-			        batch_transitions.gather(-1, (label_ids[:, t]*self.n_tags+label_ids[:, t-1]).view(-1,1)) \
-			            + feats[:, t].gather(-1, label_ids[:, t].view(-1,1)).view(-1,1)
-			return score
+		score = torch.zeros((feats.shape[0],1)).to(self.device)
+		# the 0th node is start_label->start_word,the probability of them=1. so t begin with 1.
+		for t in range(1, T):
+		    score = score + \
+		        batch_transitions.gather(-1, (label_ids[:, t]*self.n_tags+label_ids[:, t-1]).view(-1,1)) \
+		            + feats[:, t].gather(-1, label_ids[:, t].view(-1,1)).view(-1,1)
+		return score
 
 	def _viterbi_decode(self, feats):
 		'''
@@ -121,23 +119,25 @@ class Bert_BiLstm_Crf(nn.Module):
 
 	def _get_lstm_features(self, input_ids, attention_mask):
 	    """sentence is the ids"""
+	    # bert embeds: [batch_size, max_seq_len, 768]
+	    # lstm_feats: [batch_size, max_seq_len, n_tags]
+
 	    # self.hidden = self.init_hidden()
-	    embeds = self._bert_enc(input_ids, attention_mask)  # [8, 75, 768]
-	    # 过lstm
+	    embeds = self._bert_enc(input_ids, attention_mask)
 	    enc, _ = self.lstm(embeds)
 	    lstm_feats = self.fc(enc)
-	    return lstm_feats  # [8, 75, 16]
+	    return lstm_feats 
 
 	def forward(self, input_ids, attention_mask):  # dont confuse this with _forward_alg above.
 	    # Get the emission scores from the BiLSTM
-		lstm_feats = self._get_lstm_features(input_ids, attention_mask)  # [8, 180,768]
+		lstm_feats = self._get_lstm_features(input_ids, attention_mask) 
 
 	    # Find the best path, given the features.
 		score, tag_seq = self._viterbi_decode(lstm_feats)
 		return score, tag_seq
 
 	def neg_log_likelihood(self, input_ids, attention_mask, tags):
-	    feats = self._get_lstm_features(input_ids, attention_mask)  #[batch_size, max_len, 16]
+	    feats = self._get_lstm_features(input_ids, attention_mask)  #[batch_size, max_len, n_tags]
 	    forward_score = self._forward_alg(feats)
 	    gold_score = self._score_sentence(feats, tags)
 	    return torch.mean(forward_score - gold_score)
