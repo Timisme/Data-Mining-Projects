b7b55211d63b0acb87ecc813c3cc07c2ac286e2b
tim <tim@example.com>
2021-01-07 22:48:23 +0800

    hw3

--- project 3/simRank.py
+++ project 3/simRank.py
@@ -5,20 +5,17 @@
 '''
 import numpy as np 
 
-filename = 'graph_1.txt'
-
-
 class SimRank():
-	def __init__(self, filename, epoch= 5):
+	def __init__(self, filename, C= 0.8):
 
 		with open(file=filename) as f:
 
 			self.data = [ tuple(line.split(',')) for line in f.readlines()]
 
-		self.V = list(set([nodes[0] for nodes in self.data]))
-		self.E = list(set([nodes[1] for nodes in self.data]))
-
+		self.V = list(sorted(set([nodes[0] for nodes in self.data])))
+		self.E = list(sorted(set([nodes[1] for nodes in self.data])))
 		self.G = np.zeros(shape= (len(self.V), len(self.E)))
+		self.C = C
 
 		for node_tuple in self.data:
 
@@ -28,17 +25,13 @@ class SimRank():
 			v_idx = self.V.index(v)
 			e_idx = self.E.index(e)
 
-			# print(v,e)
-			# print(v_idx, e_idx)
-			# print('-'*50)
-
 			self.G[v_idx, e_idx] += 1
 
 		print(f'graph : \n{self.G}')
 		print('-'*50)
 
-		self.I_v = np.identity(len(self.V)) # 自己和自己SimRank = 1
-		self.I_e = np.identity(len(self.E))
+		self.v_SimRank = np.identity(len(self.V)) # 初始 S = I
+		self.e_SimRank = np.identity(len(self.E))
 
 	def V_SimRank(self, v1, v2):
 
@@ -51,25 +44,72 @@ class SimRank():
 			v1_idx = self.V.index(v1)
 			v2_idx = self.V.index(v2)
 
-			penalty = 1/ (self.G[v1_idx].sum(axis= 0) * self.G[v2_idx].sum(axis= 0))
+			penalty = self.C / (self.G[v1_idx].sum(axis= 0) * self.G[v2_idx].sum(axis= 0))
 			print(f'penalty: {penalty}')
 			print('-'*50)
 
-			for e_i in [ i for i, e in enumerate(self.G[v1_idx]) if e != 0]:
-				for e_j in [ j for j, e in enumerate(self.G[v2_idx]) if e != 0]:
-					if  e_i == e_j:
-						Rank_sum += 1
+			for i in [ i for i, e in enumerate(self.G[v1_idx]) if e != 0]:
+				for j in [ j for j, e in enumerate(self.G[v2_idx]) if e != 0]:
+					Rank_sum += self.e_SimRank[i, j]
+
+		return penalty * Rank_sum
+
+	def E_SimRank(self, e1, e2):
+
+		if e1 == e2:
+			return 1 
+
+		else :
+
+			Rank_sum = 0
+			e1_idx = self.E.index(e1)
+			e2_idx = self.E.index(e2)
+
+			penalty = self.C / (self.G.transpose()[e1_idx].sum(axis= 0) * self.G.transpose()[e2_idx].sum(axis= 0))
+			print(f'penalty: {penalty}')
+			print('-'*50)
+
+			for i in [ i for i, v in enumerate(self.G.transpose()[e1_idx]) if v != 0]:
+				for j in [ j for j, v in enumerate(self.G.transpose()[e2_idx]) if v != 0]:
+					Rank_sum += self.v_SimRank[i, j]
+
 		return penalty * Rank_sum
 
 
-	# def E_SimRank(self, e1, e2):
+	def Compute_SimRank(self, epochs):
+
+		for _ in range(epochs):
+
+			new_v_SimRank = np.identity(len(self.v_SimRank))
+			new_e_SimRank = np.identity(len(self.e_SimRank))
+
+			for vi in self.V:  
+				for vj in self.V: 
+					i = self.V.index(vi)
+					j = self.V.index(vj)
+					print(self.V_SimRank(v1= vi, v2= vj))
+					new_v_SimRank[i, j] = self.V_SimRank(v1= vi, v2= vj)
+
+			for ei in self.E:  
+				for ej in self.E: 
+					i = self.E.index(ei)
+					j = self.E.index(ej)
+
+					new_e_SimRank[i, j] = self.E_SimRank(e1= ei, e2= ej)
+
+			self.v_SimRank = new_v_SimRank
+			self.e_SimRank = new_e_SimRank
+
+		# print(f'SimRank: {self.v_SimRank}')
+		return self.v_SimRank
 
 def main():
 
 	Ranker = SimRank(filename= filename)
 
-	print(Ranker.V_SimRank(v1= '1', v2= '2'))
-
+	print('-'*50)
+	print('Rank: ', Ranker.Compute_SimRank(epochs= 10))
+	print('-'*50)
 
 if __name__ == '__main__':
 	main()
